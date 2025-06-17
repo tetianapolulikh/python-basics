b0fceb996725b72eecfd38919b15b0eb70194c72
tetianapolulikh <tetianapolulikh@gmail.com>
2025-06-17 07:11:50 -0400

    change 2

---
+++ chapter_07_functions/pass_the_list_to_the_function.py
@@ -0,0 +1,7 @@
+def show_messages(messages):
+    for message in messages:
+        msg = f"Your message is: {message}"
+        print(msg)
+
+list_of_messages = ['You look great today!', 'You’ve got this!', 'Make it happen!', 'Believe in yourself!', 'Stay strong!']
+show_messages(list_of_messages)
