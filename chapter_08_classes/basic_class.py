4431d9687f369ffe144129022c32ec3e3c02f53f
tetianapolulikh <tetianapolulikh@gmail.com>
2025-06-26 06:52:28 -0400

    sending files

--- chapter_08_classes/basic_class.py
+++ chapter_08_classes/basic_class.py
@@ -5,15 +5,13 @@ class Restaurant:
         self.cuisine_type = cuisine_type
 
     def describe_restaurant(self):
-        print(f'\nThe name of the restaurant is {self.restaurant_name} and there you can try { self.cuisine_type}')
+        print(f'\nThe name of the restaurant is {self.restaurant_name} and there you can try { self.cuisine_type}.')
 
     def open_restaurant(self):
         print(f'{self.restaurant_name} is opened now!')
 
-restaurant_1 = Restaurant('Sushi Box', 'chinese cuisine')
-restaurant_2 = Restaurant('Pizza Hut', 'italian cuisine')
+restaurant = Restaurant('Sushi Box', 'chinese cuisine')
 
-restaurant_1.describe_restaurant()
-restaurant_1.open_restaurant()
-restaurant_2.describe_restaurant()
-restaurant_2.open_restaurant()
+
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
--- chapter_08_classes/basic_class_different_instances.py
+++ chapter_08_classes/basic_class_different_instances.py
@@ -5,15 +5,20 @@ class Restaurant:
         self.cuisine_type = cuisine_type
 
     def describe_restaurant(self):
-        print(f'\nThe name of the restaurant is {self.restaurant_name} and there you can try { self.cuisine_type}.')
+        print(f'\nThe name of the restaurant is {self.restaurant_name} and there you can try { self.cuisine_type}')
 
     def open_restaurant(self):
         print(f'{self.restaurant_name} is opened now!')
 
 restaurant_1 = Restaurant('Sushi Box', 'chinese cuisine')
 restaurant_2 = Restaurant('Pizza Hut', 'italian cuisine')
+restaurant_3 = Restaurant('Puzata Hata', 'ukrainian cuisine')
 
 restaurant_1.describe_restaurant()
 restaurant_1.open_restaurant()
+
 restaurant_2.describe_restaurant()
 restaurant_2.open_restaurant()
+
+restaurant_3.describe_restaurant()
+restaurant_3.open_restaurant()
